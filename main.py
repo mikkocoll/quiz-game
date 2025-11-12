53cddec49397335c12a11f645638cca74045b539
mikkocoll <n12066541@qut.edu.au>
2025-11-12 23:17:47 +1000

    finalised questions

--- main.py
+++ main.py
@@ -17,3 +17,25 @@ if answer == ("foal"):
 
 else:
     print("uhhh... no.")
+
+answer = input("What sound does a horse make? ")
+if answer == ("NEIGH") or ("neigh"):
+    print("You betcha!")
+
+else:
+    print("Nay way you said that...")
+
+answer = input("How many legs does a horse usually have? ")
+if answer == ("4") or ("four"):
+    print("Absolutely!")
+
+else:
+    print("uhhh... no.")
+
+answer = input("What is female adult horse called? ")
+if answer == ("mare"):
+    print("Correct!")
+
+else:
+    print("girl...")
+
