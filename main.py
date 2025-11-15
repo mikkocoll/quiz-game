49f444360512514a007eb18f9188844b3a225573
mikkocoll <n12066541@qut.edu.au>
2025-11-15 17:42:36 +1100

    add a score to each question

--- main.py
+++ main.py
@@ -2,40 +2,40 @@ print("How well do you know horses? Let's find out!")
 
 play_game = input("Are you ready to play? ")
 
-print(play_game)
-
-if play_game != "yes":
+if play_game.lower() != "yes":
     print("????")
     quit()
 
 print("HOORAY LET'S GO!!!")
+score = 0
 
 # questions
 answer = input("What is a baby horse called? ")
-if answer == ("foal"):
+if answer.lower()  == ("foal"):
     print("Correct!")
-
+    score += 1
 else:
     print("uhhh... no.")
 
 answer = input("What sound does a horse make? ")
-if answer == ("NEIGH") or ("neigh"):
+if answer.lower()  == ("NEIGH") or ("neigh"):
     print("You betcha!")
-
+    score += 1
 else:
     print("Nay way you said that...")
 
 answer = input("How many legs does a horse usually have? ")
 if answer == ("4") or ("four"):
     print("Absolutely!")
-
+    score += 1
 else:
-    print("uhhh... no.")
+    print("try again!")
 
 answer = input("What is female adult horse called? ")
 if answer == ("mare"):
     print("Correct!")
-
+    score += 1
 else:
     print("girl...")
 
+print("You got " + str(score) + " questions correct!")
